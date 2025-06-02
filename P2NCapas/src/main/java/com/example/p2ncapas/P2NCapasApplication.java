9ee7360fd1661eb0c61d613f28e3691900b11f97
Aronpinkman <00149320@uca.edu.sv>
2025-06-02 14:02:05 -0600

    Normalizar finales de línea según .gitattributes

---
+++ P2NCapas/src/main/java/com/example/p2ncapas/P2NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.example.p2ncapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2NCapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2NCapasApplication.class, args);
+    }
+
+}
---
+++ P2NCapas/src/test/java/com/example/p2ncapas/P2NCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p2ncapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2NCapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
